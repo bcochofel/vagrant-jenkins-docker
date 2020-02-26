bbf81210a1ea0ae9f1430901f493445a39105916
Bruno Cochofel <bruno.cochofel@gmail.com>
2020-02-26 16:26:44 +0000

    install script FIX

--- install_jenkins.sh
+++ install_jenkins.sh
@@ -5,7 +5,7 @@ sudo apt install curl wget jq
 
 # create container from Dockerfile
 cd /vagrant
-docker build -t jenkins:lts-custom
+docker build -t jenkins:lts-custom .
 
 # run custom container
 docker run -p 8080:8080 -p 50000:50000 -v jenkins_home:/var/jenkins_home jenkins:lts-custom
