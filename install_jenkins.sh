a581d07f243dfdc03a1108c87093731845e04bd3
Bruno Cochofel <bruno.cochofel@gmail.com>
2020-02-27 10:14:47 +0000

    some issues fixed

--- install_jenkins.sh
+++ install_jenkins.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
+DOCKER_IMG="jenkins:lst-custom"
+
 # install tools
 sudo apt install curl wget jq
 
 # create container from Dockerfile
 cd /vagrant
-docker build -t jenkins:lts-custom .
+docker build -t ${DOCKER_IMG} .
 
 # run custom container
-docker run -d -p 8080:8080 -p 50000:50000 -v jenkins_home:/var/jenkins_home jenkins:lts-custom
+docker run -d -p 8080:8080 -p 50000:50000 -v jenkins_home:/var/jenkins_home ${DOCKER_IMG}
 
 exit 0
